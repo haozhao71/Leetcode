a2636972925cae329554558fc83c048c2cf67a68
hao.zhao <hao.zhao@thoughtworks.com>
2022-02-21 09:01:49 +0800

    [hao.zhao] Leetcode 001 Two sum

---
+++ Leetcode.Tests/TwoSumTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+
+
+namespace Leetcode.Tests
+{
+    public class TwoSumTests
+    {
+        [Fact]
+        public void Should_Throw_NullReferenceException_When_InputNums_Is_Null()
+        {
+            TwoSum twoSum = new TwoSum();
+            Action action = () => twoSum.GetResult(null, 0);
+            action.Should().Throw<ArgumentNullException>("nums should not be null");
+        }
+
+        [Theory]
+        [InlineData(new int[] { })]
+        [InlineData(new int[] { 1 })]
+        public void Should_Throw_ArrgumentException_When_InputNums_Length_Less_Than_Two(int[] nums)
+        {
+            TwoSum twoSum = new TwoSum();
+            Action action = () => twoSum.GetResult(nums, 0);
+            action.Should().Throw<ArgumentException>("nums length should more than 1");
+        }
+
+
+        [Theory]
+        [InlineData(new int[] { 1, 2 }, 3)]
+        [InlineData(new int[] { 2, 3 }, 5)]
+        public void Should_ReturnIndex_When_InputNumsLength_Is_Two(int[] nums, int target)
+        {
+            TwoSum twoSum = new TwoSum();
+            var result = twoSum.GetResult(nums, target);
+            result.Should().BeEquivalentTo(new int[] { 0, 1 });
+
+        }
+
+
+        [Theory]
+        [InlineData(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        public void Should_ReturnIndex_When_InputNumsLength_Is_MoreThanTwo(int[] nums, int target, int[] results)
+        {
+            TwoSum twoSum = new TwoSum();
+            var result = twoSum.GetResult(nums, target);
+            result.Should().BeEquivalentTo(results);
+        }
+    }
+}
---
+++ Leetcode/TwoSum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+//You can return the answer in any order.
+
+namespace Leetcode
+{
+    public class TwoSum
+    {
+        public TwoSum()
+        {
+
+        }
+
+        public int[] GetResult(int[] nums, int target)
+        {
+            if(nums == null)
+            {
+                throw new ArgumentNullException("nums should not be null");
+            }
+            if(nums.Length < 2)
+            {
+                throw new ArgumentException("nums length should more than 1");
+            }
+            Dictionary<int, int> dictionary = new Dictionary<int, int> { };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (dictionary.ContainsKey(diff))
+                {
+                    return new int[] { dictionary[diff], i};
+                }
+                else
+                {
+                    dictionary[nums[i]] = i;
+                }   
+            }
+            return new int[] { };
+        }
+    }
+}
